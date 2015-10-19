946f5a27b50cde666d02306583f3b71b8c3096db
Mary Shyne <maryshyne@Marys-MacBook-Pro.local>
2015-10-19 15:49:05 -0400

    completed homework assignment

--- mshyne-3.1.py
+++ mshyne-3.1.py
@@ -1,12 +1,7 @@
-import sys, os
+import sys, os, time
 
-directory_key = {}
-
-# Check for correct number of arguments.
-if not len(sys.argv) == 5:
-	print "Not enough arguments provided. \n \n Please use the following input criteria: \n $./list_files.py --dir='testdir' --by=mtime --results=4 --direction='descending'"
-	exit()
-
+## MAIN FUNCTIONS
+# Process directory files into a dictionary containing the number of files the user specified.
 def process_directory(directory):
 	big_ole_dict = {}
 	file_names = []
@@ -25,6 +20,7 @@ def process_directory(directory):
 			big_ole_dict[file] = { 'name': os.path.basename(fullpath), 'mtime': os.stat(fullpath).st_mtime, 'size': os.stat(fullpath).st_size}
 	return big_ole_dict
 
+# Get sorted modified time from dictionary made in process_dictionary.
 def get_mtime(processed_directory):
 	data_desired = []
 	for file in processed_directory:
@@ -33,11 +29,45 @@ def get_mtime(processed_directory):
 		return sorted(data_desired, key=lambda tup: tup[1])
 	else:
 		return sorted(data_desired, key=lambda tup: tup[1], reverse=True)
-	#return data_desired
 
+# Get sorted file size from dictionary made in process_dictionary.		
+def get_file_size(processed_directory):
+	data_desired = []
+	for file in processed_directory:
+		data_desired.append((processed_directory[file]['name'],int(processed_directory[file]['size'])))
+	if sort_direction == 'ascending':
+		return sorted(data_desired, key=lambda tup: tup[1])
+	else:
+		return sorted(data_desired, key=lambda tup: tup[1], reverse=True)	
+
+# Check for correct number of arguments.
+
+if '-h' in sys.argv:
+	print """The following convention should be used to make use of this program:
+	
+$ ./mshyne-3.1py ./directory sort_by result_number sort_direction
+
+arguments:
+	sort_by: valid arguments 'name' (sort by file name), 'size' (sort by file size), and 'mtime' (sort by last date modified)
+	result_number: how many items should be sorted from the ./directory. valid integers only.
+	sort_direction: valid arguments 'ascending' (sort by ascending value), 'descending (sort by descending value)
+
+optional argument:
+	-h: show this help message and exit
+	"""
+	exit()
+if not len(sys.argv) == 5:
+	print "Not enough arguments provided. \n \n Please use the following input criteria: \n $./list_files.py --dir='testdir' --by=mtime --results=4 --direction='descending'"
+	exit()
+
+# Collect arguments into global variables.
 directory = sys.argv[1]
 sort_criteria = sys.argv[2]
-result_number = int(sys.argv[3])
+try:
+	result_number = int(sys.argv[3])
+except ValueError:
+	print "Invalid value for result_number. Please enter an integer."
+	exit()
 sort_direction = sys.argv[4]
 
 valid_criteria = {'sort_criteria':(['mtime','size','name']), 'sort_direction':(['ascending','descending'])}
@@ -67,3 +97,20 @@ if sort_criteria == "mtime":
 	data_sorted = get_mtime(processed_directory)
 	for data in data_sorted:
 		print data[0]
+		print "Last Modified: %s" % time.ctime(data[1])
+if sort_criteria == "size":
+	data_sorted = get_file_size(processed_directory)
+	for data in data_sorted:
+		print data[0]
+		print "Size: %s bytes" % data[1]
+if sort_criteria == "name":
+	data = []
+	for key in processed_directory:
+		data.append(processed_directory[key]['name'])
+	if sort_direction == "ascending":
+		for item in sorted(data):
+			print item
+	if sort_direction == "descending":
+		for item in sorted(data, reverse=True):
+			print item
+	
