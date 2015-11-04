9f49648594fc2dbc8fa9586980e7b936569983ba
Mary Shyne <maryshyne@Marys-MacBook-Pro.local>
2015-11-04 10:21:43 -0500

    made rewrite_dict a class method

--- mylib.py
+++ mylib.py
@@ -4,25 +4,31 @@ class PersistDict(dict):
 	def __init__(self, filename):
 		dict.__init__(self)
 		self.filename = os.path.abspath(filename)
-		self.persist_dict = {}
 		if filename in os.listdir(os.path.dirname(sys.argv[0])):
-			#self.persist_dict = {}
 			read_dict = open(filename)
 			for line in read_dict.readlines():
 				key, value = line.split('\t',1)
 				self[key] = value
-			#return persist_dict
 		else:
 			open(os.path.dirname(sys.argv[0]) + "/" +  filename, 'w')
 	
 	def __setitem__(self,key,value):
 		dict.__setitem__(self,key,value)
+		self.rewrite_dict(self.filename)
 	
 	def __getitem__(self,key):
 		return dict.__getitem__(self,self.__keytransform__(key))
 	
 	def __keytransform__(self,key):
 		return key
+	
+	def rewrite_dict(self, dictionary_file):
+		temp_dict = open(dictionary_file, 'w')
+		temp_dict.write('')
+		temp_dict_2 = open(dictionary_file, 'a')
+		for key in self:
+			temp_dict_2.write(str(key) + "\t" + str(self[key]) + '\n')
+		temp_dict_2.close()
 			
 if __name__ == "__main__":
 	pass
