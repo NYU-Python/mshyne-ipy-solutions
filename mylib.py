2ebeaba47c5ffdcc5da4b8fbb9463ae4212637d9
Mary Shyne <maryshyne@Marys-MacBook-Pro.local>
2015-11-04 09:27:03 -0500

    added inherited __setitem__ definition

--- mylib.py
+++ mylib.py
@@ -6,8 +6,12 @@ class PersistDict(dict):
 		self.filename = os.path.abspath(filename)
 		if filename in os.listdir(os.path.dirname(sys.argv[0])):
 			open(os.path.dirname(sys.argv[0]) + "/" + filename, 'w')
+			
 		else:
 			open(os.path.dirname(sys.argv[0]) + "/" +  filename, 'w')
+	
+	def __setitem__(self,key,value):
+		dict.__setitem__(self,key,value)
 			
 if __name__ == "__main__":
 	pass
