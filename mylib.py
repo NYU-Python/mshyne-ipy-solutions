27a1ce13cdf89115df66e3c92e9bad6fdb2029aa
Mary Shyne <maryshyne@Marys-MacBook-Pro.local>
2015-11-04 10:03:45 -0500

    added __getitem__, __setitem__, __keytransform__ methods

--- mylib.py
+++ mylib.py
@@ -4,14 +4,25 @@ class PersistDict(dict):
 	def __init__(self, filename):
 		dict.__init__(self)
 		self.filename = os.path.abspath(filename)
+		self.persist_dict = {}
 		if filename in os.listdir(os.path.dirname(sys.argv[0])):
-			open(os.path.dirname(sys.argv[0]) + "/" + filename, 'w')
-			
+			#self.persist_dict = {}
+			read_dict = open(filename)
+			for line in read_dict.readlines():
+				key, value = line.split('\t',1)
+				self[key] = value
+			#return persist_dict
 		else:
 			open(os.path.dirname(sys.argv[0]) + "/" +  filename, 'w')
 	
 	def __setitem__(self,key,value):
 		dict.__setitem__(self,key,value)
+	
+	def __getitem__(self,key):
+		return dict.__getitem__(self,self.__keytransform__(key))
+	
+	def __keytransform__(self,key):
+		return key
 			
 if __name__ == "__main__":
 	pass
