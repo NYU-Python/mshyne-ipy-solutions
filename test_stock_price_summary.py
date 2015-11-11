133d9617591b9a6f93dcf51fef0f4c271e4a011a
Mary Shyne <maryshyne@Marys-MacBook-Pro.local>
2015-11-10 23:04:07 -0500

    successful test of is_all_numbers

--- test_stock_price_summary.py
+++ test_stock_price_summary.py
@@ -2,3 +2,6 @@ import stock_price_summary as sps
 
 def test_initial():
 	assert sps.acquire_data(filename, 5) == [114.71, 115.0, 114.32, 113.4, 115.21]
+	
+def test_is_all_numbers():
+	assert sps.is_all_numbers([1,3])
