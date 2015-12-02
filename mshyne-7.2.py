7961a27d0829291e56a1da03902dab965c77cfa1
dactampr <marycshyne@gmail.com>
2015-12-02 15:21:37 -0500

    finished 7.2 homework

--- mshyne-7.2.py
+++ mshyne-7.2.py
@@ -1,6 +1,6 @@
 import subprocess, sys
 
-#out = subprocess.check_output(['curl', sys.argv[1]])
-#print open(out).read()
+out = subprocess.check_output(['curl', sys.argv[1]])
+print out
 
-subprocess.call(['curl',sys.argv[1]], stdout=open('outfile.txt', 'w'), stderr=STDOUT)
+#subprocess.call(['curl',sys.argv[1]], stdout=open('times.html', 'w'), stderr=STDOUT)
