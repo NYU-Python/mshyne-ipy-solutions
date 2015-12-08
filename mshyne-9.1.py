2e1569bd34c084bbc1390c082b237c4568d64852
dactampr <marycshyne@gmail.com>
2015-12-08 14:19:59 -0500

    completed stock ticker app

--- mshyne-9.1.py
+++ mshyne-9.1.py
@@ -2,34 +2,25 @@ import flask
 import urllib
 import os
 
-DATABASE = 'http://finance.yahoo.com/d/quotes.csv?s={}&f=a'
+YAHOO_API = 'http://finance.yahoo.com/d/quotes.csv?s={}&f=a'
 
 app = flask.Flask(__name__) 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
-	return flask.render_templates('flask_home.html') 
+	return flask.render_template('flask_home.html') 
 
-@app.route('/display_data')            #decorator indicates web URL
-def display_data():
-	user_ticker = flask.session['ticker']
+@app.route('/get_stock', methods=['POST'])            #decorator indicates web URL
+def get_stock():
+	user_ticker = flask.request.form['ticker']
 	stock_price = build_stockprice(user_ticker)
 	return flask.render_template('flask_stock_price.html', ticker=user_ticker, price=stock_price)	
 
-@app.route('/get_stock')
-def get_stock():
-	flask.session['ticker'] = flask.request.values['ticker']
-	return flask.redirect(flask.url_for('display_data'))
-
 def build_stockprice(stock_ticker):
-	database = DATABASE.format(stock_ticker)
-	stock_price = urllib.urlopen(database).read().rstrip('\n')
+	api_link = YAHOO_API.format(stock_ticker)
+	stock_price = urllib.urlopen(api_link).read().rstrip('\n')
 	return stock_price
 
 
 if __name__ == '__main__':
     app.run(debug=True)              # app starts serving
-
-#stock_price = urllib.urlopen('http://finance.yahoo.com/d/quotes.csv?s=AAPL&f=a').read().rstrip('\n')
-
-#print stock_price
